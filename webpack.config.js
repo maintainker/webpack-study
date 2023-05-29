23f8888a3d457b1a83df118c292d324cb3ceb7ff
설영환 <devsul@jobkorea.co.kr>
2023-05-29 12:19:56 +0900

    (ssul) fix build error 수정

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,12 @@
 const path = require("path");
 const webpack = require("webpack");
+
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+process.env.NODE_ENV = process.env.NODE_ENV || "development";
+
 module.exports = {
   mode: "development",
   entry: {
@@ -15,49 +19,53 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /\.css$/,
+        test: /\.(scss|css)$/,
         use: [
           process.env.NODE_ENV === "production"
             ? MiniCssExtractPlugin.loader // 프로덕션 환경
-            : "style-loader",
+            : "style-loader", // 개발 환경
           "css-loader",
         ],
-        // 개발 환경,],
       },
       {
         test: /\.(png|jpg|svg|gif)$/,
-        use: [
-          {
-            loader: "url-loader",
-            options: {
-              name: "[name].[ext]?[hash]",
-              limit: 10000, // 10Kb
-            },
-          },
-        ],
+        loader: "url-loader",
+        options: {
+          name: "[name].[ext]?[hash]",
+          limit: 10000, // 10Kb
+        },
       },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: "babel-loader", // 바벨 로더를 추가한다
+      },
+      /**
+       * TODO: babel-loader를 구성해 보세요.
+       */
     ],
   },
   plugins: [
     new webpack.BannerPlugin({
-      banner: `build start: (${new Date().toLocaleString()})`,
+      banner: `빌드 날짜: ${new Date().toLocaleString()}`,
     }),
     new HtmlWebpackPlugin({
       template: "./src/index.html",
       templateParameters: {
         env: process.env.NODE_ENV === "development" ? "(개발용)" : "",
       },
+      minify:
+        process.env.NODE_ENV === "production"
+          ? {
+              collapseWhitespace: true, // 빈칸 제거
+              removeComments: true, // 주석 제거
+            }
+          : false,
+      hash: process.env.NODE_ENV === "production",
     }),
     new CleanWebpackPlugin(),
     ...(process.env.NODE_ENV === "production"
       ? [new MiniCssExtractPlugin({ filename: `[name].css` })]
       : []),
   ],
-  /**
-   * TODO: 아래 플러그인을 추가해서 번들 결과를 만들어 보세요.
-   * 1. BannerPlugin: 결과물에 빌드 시간을 출력하세요. --
-   * 2. HtmlWebpackPlugin: 동적으로 html 파일을 생성하세요. --
-   * 3. CleanWebpackPlugin: 빌드 전에 아웃풋 폴더를 깨끗히 정리하세요.
-   * 4. MiniCssExtractPlugin: 모듈에서 css 파일을 분리하세요.
-   */
 };
